641d102ed856ffa15886f8cbc9f4393361ab2507
Shaik Bunglow Samreen <shaiksamreensam123@gmail.com>
2024-02-13 23:15:32 +0530

    main

---
+++ Conditional_stat.java
@@ -0,0 +1,19 @@
+/*Conditional Statements ‘if-else’
+The if block is used to specify the code to be executed if the condition specified
+in if is true, the else block is executed otherwise.*/
+import java.util.*;
+public class Conditional_stat {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        
+        // if condition checks weather condition is true or not if it is true then the  if condition will be printed
+        if(age>=18){
+            System.out.println("The person is adult");
+        }
+        // if codition is not true the else statement will be printed
+        else{
+            System.out.println("The pearson is not adult");
+        }
+    }
+}
---
+++ User_input.java
@@ -0,0 +1,19 @@
+// Import java package
+import java.util.*;
+public class User_input {
+    // Scanner class takes user input
+
+    Scanner sc=new Scanner(System.in);
+    
+    // nextline() uses take user input as string
+    String name=sc.nextLine();
+
+    // nextInt() is used to take user input as integer
+    int a=sc.nextInt();
+
+    // nextFloat is used to print user input as float
+    float b=sc.nextFloat();
+
+    // 
+
+}
